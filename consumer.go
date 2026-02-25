2f26388dccf0920099794c3897790889e7e2a120
Jochen Schalanda <jochen@schalanda.name>
2026-02-25 09:32:58 +0100

    chore: remove duplicate code in consumeMessages (#128)

--- consumer.go
+++ consumer.go
@@ -510,9 +510,6 @@ func (c *Consumer) consumeMessages(ctx context.Context, query *query.Builder) ([
 			if c.cfg.FiniteConsumption && errors.Is(err, pgx.ErrNoRows) {
 				return nil, io.EOF
 			}
-			if c.cfg.FiniteConsumption && errors.Is(err, pgx.ErrNoRows) {
-				return nil, io.EOF
-			}
 			if !errors.Is(err, pgx.ErrNoRows) {
 				return nil, errors.WithStack(err)
 			}
